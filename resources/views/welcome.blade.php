38a7b86cb9dc98ac70dc87eb17b552e27cf4ff99
cokoyoh <charlesokoyoh@gmail.com>
2019-10-14 10:36:56 +0300

    Update the current welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,16 +30,13 @@
 
     <div class="min-h-screen flex items-center justify-center">
         <div class="flex flex-col justify-around h-full">
-            <div>
-                <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
-                    {{ config('app.name', 'Laravel') }}
-                </h1>
-                <p class="font-hairline text-gray-800 text-2xl">
-{{--                <p class="font-hairline my-heading">--}}
-                    This is a million dollar app <br>
-                    <span class="text-sm font-hairline ml-16">Signed cokoyoh</span>
-                </p>
-            </div>
+            <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
+                {{ config('app.name', 'Laravel') }}
+            </h1>
+            <p class="font-hairline text-gray-800 text-center text-2xl">
+                This is a million dollar app <br>
+                <span class="text-sm font-hairline">Signed by cokoyoh</span>
+            </p>
         </div>
     </div>
 </div>

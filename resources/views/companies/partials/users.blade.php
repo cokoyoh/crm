0588d1740aae589228f7e0dfe566ef5841c51744
cokoyoh <charlesokoyoh@gmail.com>
2019-10-23 11:52:21 +0300

    Customise the crm-table styles

--- resources/views/companies/partials/users.blade.php
+++ resources/views/companies/partials/users.blade.php
@@ -3,89 +3,36 @@
     <a href="{!! route('users.invite', $company) !!}" class="btn btn-blue ml-4">Add User</a>
 </div>
 
-<div class="flex justify-between w-full card mt-4">
-    <div class="px-3 py-4 flex justify-center">
-        <table class="table-auto w-full text-md shadow rounded mb-4">
-            <tbody>
-            <tr class="border-b">
-                <th class="text-left p-3 px-5">Name</th>
-                <th class="text-left p-3 px-5">Email</th>
-                <th class="text-left p-3 px-5">Role</th>
-                <th class="text-left p-3 px-5">Status</th>
-                <th class="text-left p-3 px-5">Action</th>
-                <th></th>
-            </tr>
-            <tr class="border-b hover:bg-orange-100 bg-gray-100">
-                <td class="p-3 px-5"><input type="text" value="user.name" class="bg-transparent"></td>
-                <td class="p-3 px-5"><input type="text" value="user.email" class="bg-transparent"></td>
-                <td class="p-3 px-5">
-                    <select value="user.role" class="bg-transparent">
-                        <option value="user">user</option>
-                        <option value="admin">admin</option>
+<div class="flex justify-between w-full mt-4 py-4">
+    <table class="crm-table table-auto">
+        <tbody>
+        <tr class="border-b">
+            <th class="text-left p-3 px-5">Name</th>
+            <th class="text-left p-3 px-5">Email</th>
+            <th class="text-left p-3 px-5">Role</th>
+            <th class="text-left p-3 px-5">Status</th>
+            <th class="text-left p-3 px-5">Action</th>
+            <th></th>
+        </tr>
+        @foreach($users as $user)
+            <tr class="border-b hover:bg-gray-200" :class="{!! 1 == 2 ? 'bg-gray-100' : 'bg-red-200' !!}">
+                <td class="p-3 px-5">{!! $user->name !!}</td>
+                <td class="p-3 px-5">{!! $user->email !!}</td>
+                <td class="td p-3 px-5">
+                    <select class="bg-transparent">
+                        @foreach($user->roles() as $roleUser)
+                            <option value="{!! $roleUser->id !!}" class="td">{!! $roleUser->role->name !!}</option>
+                        @endforeach
                     </select>
                 </td>
-                <td class="p-3 px-5"><input type="text" value="user.status" class="bg-transparent"></td>
+                <td class="p-3 px-5">{!! $user->status !!}</td>
                 <td class="p-3 px-5 flex justify-end">
-                    <button type="button"
-                            class="text-xs bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded-lg focus:outline-none focus:shadow-outline">
+                    <button type="button" class="btn-sm btn-danger">
                         Delete
                     </button>
                 </td>
             </tr>
-            <tr class="border-b hover:bg-orange-100">
-                <td class="p-3 px-5"><input type="text" value="user.name" class="bg-transparent"></td>
-                <td class="p-3 px-5"><input type="text" value="user.email" class="bg-transparent"></td>
-                <td class="p-3 px-5">
-                    <select value="user.role" class="bg-transparent">
-                        <option value="user">user</option>
-                        <option value="admin">admin</option>
-                    </select>
-                </td>
-                <td class="p-3 px-5"><input type="text" value="user.status" class="bg-transparent"></td>
-                <td class="p-3 px-5 flex justify-end">
-                    <button type="button"
-                            class="text-xs bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded-lg focus:outline-none focus:shadow-outline">
-                        Delete
-                    </button>
-                </td>
-            </tr>
-
-            <tr class="border-b hover:bg-orange-100">
-                <td class="p-3 px-5"><input type="text" value="user.name" class="bg-transparent"></td>
-                <td class="p-3 px-5"><input type="text" value="user.email" class="bg-transparent"></td>
-                <td class="p-3 px-5">
-                    <select value="user.role" class="bg-transparent">
-                        <option value="user">user</option>
-                        <option value="admin">admin</option>
-                    </select>
-                </td>
-                <td class="p-3 px-5"><input type="text" value="user.status" class="bg-transparent"></td>
-                <td class="p-3 px-5 flex justify-end">
-                    <button type="button"
-                            class="text-xs bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded-lg focus:outline-none focus:shadow-outline">
-                        Delete
-                    </button>
-                </td>
-            </tr>
-            <tr class="border-b hover:bg-orange-100 bg-gray-100">
-                <td class="p-3 px-5"><input type="text" value="user.name" class="bg-transparent"></td>
-                <td class="p-3 px-5"><input type="text" value="user.email" class="bg-transparent"></td>
-                <td class="p-3 px-5">
-                    <select value="user.role" class="bg-transparent">
-                        <option value="user">user</option>
-                        <option value="admin">admin</option>
-                    </select>
-                </td>
-                <td class="p-3 px-5"><input type="text" value="user.status" class="bg-transparent"></td>
-                <td class="p-3 px-5 flex justify-end">
-                    <button type="button"
-                            class="text-xs bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded-lg focus:outline-none focus:shadow-outline">
-                        Delete
-                    </button>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
-
+        @endforeach
+        </tbody>
+    </table>
 </div>

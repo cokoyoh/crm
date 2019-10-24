77673eb70b1ac1ce803201252b818edcb755d30a
cokoyoh <charlesokoyoh@gmail.com>
2019-10-24 17:21:18 +0300

    Update the svgs

--- resources/views/dashboards/recent/recently_added_users.blade.php
+++ resources/views/dashboards/recent/recently_added_users.blade.php
@@ -8,7 +8,7 @@
                 <h6 class="text-blue-600 text-sm">{!! $user->name !!}</h6>
                 <p class="text-gray-600 text-xs italic">{!! $user->company->name !!}</p>
             </div>
-            <svg class="h-4 w-4" viewBox="0 0 20 20">
+            <svg class="h-4 w-4 fill-current text-gray-700" viewBox="0 0 20 20">
                 <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-1-7.59V4h2v5.59l3.95 3.95-1.41 1.41L9 10.41z"/>
             </svg>
         </div>

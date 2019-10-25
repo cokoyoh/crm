c53649143cea3d117ceba6ee28b1fca2982250a3
cokoyoh <charlesokoyoh@gmail.com>
2019-10-25 03:58:40 +0300

    Align file contents

--- resources/views/dashboards/super_admin.blade.php
+++ resources/views/dashboards/super_admin.blade.php
@@ -1,33 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-{{--    <p>Some lubba dubba dub dub...</p>--}}
-
-{{--   the header --}}
-
-
-
-{{--    main section --}}
-
-{{--    --}}{{--    left side --}}
-{{--        - see a list of registered companies--}}
-{{--        - display companies in a table--}}
-{{--        - see total users--}}
-{{--        ---}}
-
-
-{{--    --}}{{--    right side --}}
-{{--        - Today's schedules--}}
-{{--        - Upcoming schedules--}}
-{{--        - Recently added companies--}}
-{{--        ---}}
-
-
-{{--    end of main section--}}
-
-    <div class="container mx-auto">
-        @include('.dashboards.superadmin.header')
-        @include('.dashboards.superadmin.main')
-    </div>
-
+<div class="container mx-auto">
+    @include('.dashboards.superadmin.header')
+    @include('.dashboards.superadmin.main')
+</div>
 @endsection

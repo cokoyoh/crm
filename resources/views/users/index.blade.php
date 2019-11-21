7a9c10fa934d1c66e6d2a17b0ac6c4fa2884bf64
cokoyoh <charlesokoyoh@gmail.com>
2019-11-21 06:08:42 +0300

    Link the route the create a company user for the company admin

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -41,18 +41,18 @@
                     </svg>
                 </button>
             </span>
-
-                    <button
-                        @click="$modal.show('new-lead-source-modal')"
-                        class="ml-5 flex items-center pr-4 pl-2 py-2 text-sm font-medium text-white bg-gray-800 hover:bg-gray-700 rounded focus:outline-none active:bg-gray-700">
-                        <svg
-                            class="fill-current h-4 w-4"
-                            viewBox="0 0 20 20">
-                            <path
-                                d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/>
-                        </svg>
-                        <span class="ml-1">User</span>
-                    </button>
+                    <a href="{!! route('users.invite', auth()->user()->company) !!}">
+                        <button
+                            class="ml-5 flex items-center pr-4 pl-2 py-2 text-sm font-medium text-white bg-gray-800 hover:bg-gray-700 rounded focus:outline-none active:bg-gray-700">
+                            <svg
+                                class="fill-current h-4 w-4"
+                                viewBox="0 0 20 20">
+                                <path
+                                    d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/>
+                            </svg>
+                            <span class="ml-1">User</span>
+                        </button>
+                    </a>
                 </div>
             </div>
         </header>
